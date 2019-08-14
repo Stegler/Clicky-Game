4d2f818f65078acd8598bdb1ce084c8447d4174e
David Steger <d.steger@ymail.com>
2019-08-13 18:40:06 -0700

    Updates

---
+++ precache-manifest.00fb43f40acc857a385acf7457dc1bc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea5485df88fc206c3b7acb39782b81c",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "0125d9c89fdb6b4bfc7f",
+    "url": "/Clicky-Game/static/css/main.15cc2b2a.chunk.css"
+  },
+  {
+    "revision": "b524c3c443a56276437d",
+    "url": "/Clicky-Game/static/js/2.de57ce6a.chunk.js"
+  },
+  {
+    "revision": "0125d9c89fdb6b4bfc7f",
+    "url": "/Clicky-Game/static/js/main.4a866680.chunk.js"
+  },
+  {
+    "revision": "bd79fc15be5aba147ee5",
+    "url": "/Clicky-Game/static/js/runtime~main.7329c7d0.js"
+  }
+]);
